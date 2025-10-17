9bd6d3e5eb2373ba81a5e8319f86ae657a6e8caa
kshy233 <51500612+armjukl@users.noreply.github.com>
2025-10-18 02:51:46 +0800

    Add postcss-import and cssnano for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+// postcss.config.js
 module.exports = {
   plugins: {
+    'postcss-import': {}, // 如果你需要 @import 支持
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? {
+      cssnano: { // 生产环境压缩 CSS
+        preset: 'default',
+      }
+    } : {})
   },
 }
